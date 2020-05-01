5b7101978bfe40955f79812803fa944713752c54
obiwanknabe <59886076+obiwanknabe@users.noreply.github.com>
2020-05-01 12:14:35 -0400

    JS tutorial video exercise: primitive/value types

---
+++ primitive_types.js
@@ -0,0 +1,7 @@
+let name = 'Mosh'; //String Literal 
+let age = 30; // Number Literal
+let selected_color = blue;
+let selected_color = null;
+let isApproved = false // Boolean
+let firstName = undefined;
+
