8614bfade94992ad2903ade6a5c82d1666f5f4b1
obiwanknabe <59886076+obiwanknabe@users.noreply.github.com>
2020-05-01 12:33:46 -0400

    JS tutorial video exercise: objects

---
+++ referencetypes_object.js
@@ -0,0 +1,18 @@
+let name = 'Lauren';
+let age = 20;
+
+let person = {
+    name: 'Lauren',
+    age:   20
+}; 
+// Dot Notation
+person.name = 'Lauren';
+
+// Bracket Notation 
+person['name'] = 'Mary';
+
+let selection = 'name';
+person[selection] = 'Mary';
+
+
+console.log(person);
