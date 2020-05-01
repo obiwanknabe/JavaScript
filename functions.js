09cdaf09e52a4c3a330fc3011c8cb79719b014bc
obiwanknabe <59886076+obiwanknabe@users.noreply.github.com>
2020-05-01 12:53:02 -0400

    JS tutorial video exercise: functions

---
+++ functions.js
@@ -0,0 +1,16 @@
+// Performing a task
+function greet(name, lastName) {
+    console.log('Hello ' + name + lastName);
+} 
+greet('John ', 'Smith');  
+greet('Mary ');
+
+// Calculating a value
+function square(number) {
+    return number * number;
+}
+
+let number = square(2);
+console.log(number);
+console.log(square(2))
+
